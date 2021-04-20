fd7b6ff102bfd7baeb245d6277b6fe9065418417
nassersaazi <nassersnoop@gmail.com>
2021-04-20 16:20:42 +0300

    Done adding mobile menu for Easybank SASS project

--- projects/easybank/app/js/script.js
+++ projects/easybank/app/js/script.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body'); 
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if (header.classList.contains('open')) {
+        body.classList.remove('no-scroll');
+        header.classList.remove('open');// close hamburger menu
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        })
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
+    } else {
+        body.classList.add('no-scroll');
+        header.classList.add('open');// open hamburger menu
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+})
